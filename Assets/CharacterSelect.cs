75fdf79ac675b99f59f9461af04aa364cfd4a865
JLaw301 <75862922+JLaw301@users.noreply.github.com>
2024-11-19 02:25:23 -0500

    Updated Character and Added Character Selection Screen
    All Character Scripts are now children of the base character script
    
    All animations are code based and no longer graph based
    
    Added new Character Select Screen. Players 1 and 2 can select a box and the gameplay scene will open.

---
+++ Assets/CharacterSelect.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject slotGrid;
+    public Image p1_CharacterImage;
+    public Text p1_CharacterNameText;
+    public Image p2_CharacterImage;
+    public Text p2_CharacterNameText;
+    public int maxSlots;
+    public int p1_Current_Slot = 0;
+    public int p2_Current_Slot = 1;
+    public GameObject p1_selectedCharacter;
+    public GameObject p2_selectedCharacter;
+    public bool isP1Selected = false;
+    public bool isP2Selected = false;
+    public UnityEvent P1Selected_Event;
+    public UnityEvent P2Selected_Event;
+
+    public List<GameObject> slots;
+    void Start()
+    {
+        maxSlots = slotGrid.transform.childCount;
+        
+        foreach (Transform child in slotGrid.gameObject.transform)
+        {
+            slots.Add(child.gameObject);
+        }
+
+        //GameManager.gameManager.p1_character_prefab = null;
+        //GameManager.gameManager.p2_character_prefab = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        HandleSlot();
+        HandleCharacterDisplay();
+
+        if(isP1Selected && isP2Selected){
+            isP1Selected = false;
+            isP2Selected = false;
+            //GameManager.gameManager.p1_character_prefab = p1_selectedCharacter;
+            //GameManager.gameManager.p2_character_prefab = p2_selectedCharacter;
+            SceneManager.LoadScene("SampleScene");
+            print("LoadNewScene");
+        }
+    }
+
+    public void HandleInput(){
+        // Player 1 Controls
+        if(Input.GetKeyDown(KeyCode.A) && !isP1Selected){
+            p1_Current_Slot = math.clamp(p1_Current_Slot-1, 0, maxSlots-1);
+        }
+        if(Input.GetKeyDown(KeyCode.D) && !isP1Selected){
+            p1_Current_Slot = math.clamp(p1_Current_Slot+1, 0, maxSlots-1);
+        }
+        if(Input.GetKeyDown(KeyCode.W) && !isP1Selected){
+            if (p1_Current_Slot-5 < 0){return;}
+            p1_Current_Slot = p1_Current_Slot-5;
+        }
+        if(Input.GetKeyDown(KeyCode.S) && !isP1Selected){
+            if (p1_Current_Slot+5 > maxSlots-1){return;}
+            p1_Current_Slot = p1_Current_Slot+5;
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if (isP1Selected){
+                isP1Selected= false;
+                p1_selectedCharacter = null;
+            }
+            else{
+                //p1_selectedCharacter = slots[p1_Current_Slot].GetComponent<SlotCharacterData>().characterPrefab;
+                isP1Selected = true;
+                P1Selected_Event.Invoke();
+            }
+        }
+
+        // Player 2 Controls
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && !isP2Selected){
+            p2_Current_Slot = math.clamp(p2_Current_Slot-1, 0, maxSlots-1);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow) && !isP2Selected){
+            p2_Current_Slot = math.clamp(p2_Current_Slot+1, 0, maxSlots-1);;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && !isP1Selected){
+            if (p2_Current_Slot-5 < 0){return;}
+            p2_Current_Slot = p2_Current_Slot-5;
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow) && !isP1Selected){
+            if (p2_Current_Slot+5 > maxSlots-1){return;}
+            p2_Current_Slot = p2_Current_Slot+5;
+        }
+        if(Input.GetKeyDown(KeyCode.RightControl)){
+           if (isP2Selected){
+                isP2Selected = false;
+                p2_selectedCharacter = null;
+            }
+            else{
+                //p2_selectedCharacter = slots[p2_Current_Slot].GetComponent<SlotCharacterData>().characterPrefab;
+                isP2Selected = true;
+                P2Selected_Event.Invoke();
+            }
+        }
+    }
+
+    public void HandleSlot(){
+        // Change Slot Color
+        foreach (GameObject slot in slots)
+        {
+            if (slots.IndexOf(slot) != p1_Current_Slot && slots.IndexOf(slot) != p2_Current_Slot){
+                slots[slots.IndexOf(slot)].GetComponent<Image>().color = Color.black;
+            }
+            else if(p2_Current_Slot == p1_Current_Slot){
+                slots[p1_Current_Slot].GetComponent<Image>().color = Color.magenta;
+                slots[p2_Current_Slot].GetComponent<Image>().color = Color.magenta;
+            }
+            else if(slots.IndexOf(slot) == p1_Current_Slot){
+                slots[p1_Current_Slot].GetComponent<Image>().color = Color.red;
+            }
+            else if(slots.IndexOf(slot) == p2_Current_Slot){
+                slots[p2_Current_Slot].GetComponent<Image>().color = Color.blue;
+            }
+        
+        }
+    }
+
+    public void HandleCharacterDisplay(){
+        //p1_CharacterImage.sprite = slots[p1_Current_Slot].GetComponent<SlotCharacterData>().characterIcon;
+        //p2_CharacterImage.sprite = slots[p2_Current_Slot].GetComponent<SlotCharacterData>().characterIcon;
+
+        //p1_CharacterNameText.text = slots[p1_Current_Slot].GetComponent<SlotCharacterData>().characterName;
+        //p2_CharacterNameText.text = slots[p2_Current_Slot].GetComponent<SlotCharacterData>().characterName;
+
+        // Random Select Display
+        // if (p2_CharacterNameText.text == "Random" && p2_CharacterImage.transform.localScale != new Vector3(1,1,1)){
+        //     p2_CharacterImage.transform.localScale = new Vector3(1,1,1);
+        // }
+        // else if (p2_CharacterNameText.text != "Random" && p2_CharacterImage.transform.localScale == new Vector3(1,1,1)){
+        //     p2_CharacterImage.transform.localScale = new Vector3(-1,1,1);
+        // }
+
+    }
+
+}
---
+++ Assets/Characters/Bean/Bean_BC.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bean_BC : BaseCharacter
+{
+    // Start is called before the first frame update
+    protected override void Start() {
+        base.Start();
+        characterName = "Bean";
+    }
+}
+
+    
